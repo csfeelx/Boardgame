6571c430e816edd1d287518507ec986e8713c3af
DESKTOP-OH4RS9J\csp <csp@pubple.com>
2018-10-25 18:13:09 +0900

    no message

--- data.js
+++ data.js
@@ -6,25 +6,25 @@ var data = [
                         cat: "1a",
                         name: "Expedition into the Wild",
                         en: "Your expedition has ventured far into the rainforests of southern Mexico in search of a forgotten Aztec city-state. What will you find when you head deeper into the untamed jungle?",
-                        ko: "당신의 원정대는 잊혀진 아즈텍 도시 국가를 찾기 위해 멕시코 남부의 열대 우림으로 멀리갔습니다. 길들이지 않은 밀림 속으로 더 깊이 들어가면 무엇을 발견 할 수 있을까요?"
+                        ko: "당신의 원정대는 잊혀진 아즈텍 도시 국가를 찾아 멕시코 남부의 열대 우림으로 긴 여행을 했다. 문명의 자취가 없는 밀림의 깊은 곳에서 무엇을 찾아낼 수 있을까?"
                 },
                 {
                         cat: "1b",
                         name: "The Serpents Attack!",
                         en: "After hours of exploration and futile attempts to map out the wilds, you're convinced something is watching your progress, stalking your every move. The shapes of large serpentine creatures slither in the brush all around you. You try to back away, but the creatures have you completely surrounded, hissing at you from all sides. Suddenly, several shapes emerge from the brush: human-sized creatures with the bodies of serpents and arms wielding spears and jagged knives. The creatures are savage, attacking you fiercely. Somehow, you manage to flee to safety, though the sensation that you are being watched remains. Although you survive the encounter, you are confronted with a stark reality: this place is home to creatures beyond your understanding.",
-                        ko: "몇 시간의 탐험과 야생을 찾아내는 쓸데없는 시도 후에, 당신은 당신의 모든 움직임을 스토킹하면서 당신의 진보를 지켜보고 있다고 확신합니다. 커다란 사문석 생물의 모양이 당신 주변의 칫솔에서 미끄러 져 버립니다. 당신은 뒤로 물러나려고 노력하지만, 생물들은 당신을 완전히 둘러싸고 있습니다. 갑자기 뱀에서 몇 가지 모양이 나옵니다. 인간 크기의 생물로 뱀과 팔이 창을 휘두르고 날카로운 칼을 휘감습니다. 생물들은야만스럽고 당신을 맹렬히 공격합니다. 여하튼, 당신이 지켜보고있는 감정은 남아 있지만, 당신은 안전을 피해 도망 칠 수 있습니다. 당신이 만남에서 살아남지 만, 당신은 뻔한 현실에 직면 해 있습니다.이 장소는 당신의 이해를 넘어선 생물의 고향입니다."
+                        ko: "몇 시간 동안이나 헤매이며 야생 지역의 지도를 그리려는 헛된 시도 후에, 당신은 무언가가 당신의 모든 발걸음을 쫓으며, 당신의 여정을 지켜보고 있다는 것을 확신했다. 커다란 뱀 같은 생물체의 형상이 당신을 둘러싼 덤불 속에서 미끄러지며 움직이고 있다. 당신은 도망치려 했지만, 그 생물들이 이미 당신을 포위하였는지 주변에선 온통 쉿쉿대는 소리가 들려오고 있다. 그때 덤불 속에서 그 생물 몇이 모습을 드러내었다. 그것은 창과 날카로운 단도로 무장하고 있는 뱀의 몸을 가진, 사람 크기의 생물이다. 그 야만스러운 생물들은 당신에게 맹렬한 공격을 시작했다. 당신은 어떻게든 안전하게 도망치는 것에 성공했지만, 여전히 그들의 추적을 벗어나지 못 했다는 것을 알 수 있다. 당장은 위험에서 벗어났지만, 당신은 끔찍한 사실을 깨달았다. 바로 이 장소는 당신이 상상도 못했던 그 생물들의 서식처라는 것을."
                 },
                 {
                         cat: "2a",
                         name: "Intruders",
                         en: "What was once excitement has turned to dread. This place is far deadlier than you could have imagined. It is as though the jungle itself wants you out.",
-                        ko: "한때 흥분했던 것이 두려운 것이되었습니다. 이 곳은 당신이 상상했던 것보다 훨씬 치명적입니다. 그것은 마치 정글 그 자체가 당신을 원한다는 것입니다."
+                        ko: "모험의 흥분은 이제 공포가 되었다. 이 곳은 당신이 상상했던 그 무엇보다 위험한 곳이다. 마치 이 밀림 자체가 당신을 쫓아내려 하는 것 같다."
                 },
                 {
                         cat: "2b",
                         name: "Endless Meandering",
                         en: "You find yourself in an endless loop, traversing the same path through the jungle over and over again. No matter which direction you walk, the result is the same. It has been hours, perhaps even days. Meanwhile, you can hear the serpent creatures hissing in the trees, mocking you from afar. Out of food and exhausted from lack of sleep, you collapse against a half-rotted tree. Your vision becomes hazy.",
-                        ko: "당신은 끝없이 반복되어 반복적으로 정글을 통과하는 같은 길을지나갑니다. 어떤 방향으로 걸어가더라도 결과는 같습니다. 아마도 몇 시간이 걸렸을 것입니다. 그 사이에 나무에서 뱀의 생물들이 쉿 소리를 내며 멀리서 조롱하는 소리를들을 수 있습니다. 음식에서 벗어나 수면 부족으로 지쳐서, 당신은 반 썩은 나무에 붕괴됩니다. 당신의 시야가 흐릿 해집니다."
+                        ko: "당신은 모두 다 같아 보이는 밀림의 길을 걷고 또 걸으며 끝없이 헤매이고 있는 자신을 발견했다. 당신이 어느 쪽으로 방향을 정하더라도 결과는 같았다. 이것은 몇 시간 동안 계속되었다, 아니. 며칠일지도. 이 시간 동안 당신은 나무 위에서 쉿쉿대는 뱀 같은 생물들이 멀리서부터 당신을 조롱하는 소리를 들을 수 있었다. 수면 부족으로 인한 피로와 굶주림으로 당신은 반쯤 썩어가는 나무에 부딪혀 쓰러졌다. 당신의 시야가 흐릿해졌다."
                 }
                 ],
                 act: [
@@ -32,13 +32,13 @@ var data = [
                         cat: "1a",
                         name: "Exploring the Rainforest",
                         en: "Today, you depart from your camp in order to find the ruins Alejandro seeks. That is, if they even exist…",
-                        ko: "오늘 알레한드로가 찾는 유적을 찾기 위해 당신은 당신의 야영지에서 출발합니다. 그들이 존재하더라도 ..."
+                        ko: "오늘, 당신은 알레한드로가 이야기한 유적을 찾기 위해 야영지에서 출발한다. 그들이 존재한다면..."
                 },
                 {
                         cat: "1b",
                         name: "The Natives",
                         en: "As you explore the dense jungle, you are suddenly confronted by several natives. At first glance, they appear to be warriors of an Aztec city-state: perhaps even the one you seek. But how could they have survived undiscovered for this long? Even with Alejandro's notes, you are barely able to translate what the warriors say, only that they call you \"intruder\" and implore you to leave the jungle at once. You try to explain that you are searching for the \"Eztli,\" which causes them to murmur and debate among themselves. Finally, one of them speaks to you in rudimentary Spanish. 'Ichtaca will know what to do with you, intruder.\" He blows on a decorated clay ocarina, producing a shrill note that echoes throughout the jungle. The warriors depart as suddenly as they arrived.",
-                        ko: "밀도가 높은 정글을 탐험하면서 갑자기 여러 원주민이 직면하게됩니다. 언뜻 보면, 그들은 아즈텍 도시 국가의 전사 인 것처럼 보입니다. 아마도 당신이 찾는 사람조차도. 그러나 어떻게이 사람들은 오래 동안 발견되지 않은 채 살아남을 수 있었 을까? Alejandro의 노트를 사용하더라도 전사자의 말을 간신히 번역 할 수 있습니다. 단지 \"침입자\"라고 말하면서 곧바로 밀림을 떠나라고 당부합니다. 당신은 \"Eztli\"를 찾고 있다는 설명을하려고합니다. Eztli는 그들 사이에 불평과 논쟁을 불러 일으 킵니다. 마침내, 그들 중 하나가 초보적인 스페인어로 당신에게 이야기합니다. \"Ichtaca는 침입자, 당신과 무엇을 해야할지 알 것입니다.\" 그는 장식 된 진흙 오카리나를 불고 정글 전체에 울려 퍼지는 신나는 음표를냅니다. 전사들은 그들이 도착했을 때 갑자기 떠난다."
+                        ko: "울창한 밀림을 탐험하던 중 당신은 몇몇 원주민과 마주쳤다. 언뜻 보기에 그들은 아즈텍 도시 국가의 전사로 보인다. 어쩌면 그들 중 당신이 찾는 이가 있을 수도 있다. 궁금한것은 그들이 어떻게 이토록 오래 동안 발견되지 않은 채 살고 있었던 것일까 하는 점이다. 알레한드로의 노트를 참고하여 겨우 그들이 말하는 것의 일부를 알아들을 수 있었다. 그것은 그들이 당신을 \"침입자\"라 부르며 당장 이 밀림에서 나가라고 하는 내용이었다. 당신은 그들에게 당신이 \"에즈틀리\"를 찾고 있다는 것을 설명하려 했고, 이를 들은 그들은 서로 무언가 이야기를 주고 받았다. 마침내 그들 중 한 명이 미숙한 스페인어로 이야기 했다. \"이차카가 너희를 어떻게 해야할지 알 것이다. 침입자.\" 그가 장식이 있는 도자기 오카리나를 불자, 날카로운 소리가 온 밀림에 메아리친다. 전사들은 그들이 나타났을 때와 마찬가지로 순식간에 사라졌다."
                 },
                 {
                         cat: "2a",
