50d1d2bd9c13824d78f961c0addb9e210eb898b6
DESKTOP-OH4RS9J\csp <csp@pubple.com>
2018-11-21 01:34:20 +0900

    no message

--- data_dragonfire.js
+++ data_dragonfire.js
@@ -1,5 +1,20 @@
 var data_dragonfire = [
         {
+                name: "# DUNGEON CRAWL",
+                en: "",
+                ko: "마을 주민들은 겁에 질려있다. 마을 근처의 미궁에 서식하는 흉포한 괴물들이 지상을 습격하고, 논밭을 불 태우고, 교역을 방해하며, 주민들을 잡아간다. 마을은 용감한 모험가들을 필요로 한다."
+        },
+        {
+                name: "# THE PORTCULLIS IS BREACHED",
+                en: "",
+                ko: "적들이 벽을 넘어오며 요새를 난도질하고 있다. 당신은 요새 관문이 파괴되기 전에 이들을 물리치고 요새를 지켜낼 수 있는가?"
+        },
+        {
+                name: "# THE WASTES OF ANCIENT EMPIRE",
+                en: "",
+                ko: "몬스터와 고대의 뼈들이 데사린 계곡을 어지럽히고 있다. 그리고 이 황야의 어딘가 텔럼즈 홀드의 상속자인 텔럼경의 아들이 곤경에 처해있다."
+        },
+        {
                 name: "THE WASTES OF AN ANCIENT EMPIRE: PROLOGUE",
                 en: "The Dessarin Valley.<br/><br/>Stunted brush and thorn-laden vines camouflage ancient foundations and work to reclaim broken towers. Burned land from a recent forest fire leaves charred bones for scavengers to pick through. Over a rise, hunting paths for the Valley's scavengers twist and turn through briar thickets and poisoned berry-brush.<br/><br/>\“Too much blood spilled here.\"<br/><br/>That's the common wisdom, and you don't have to look far for evidence. Eyes watch from the deeper shadows of the wilderness. The carcasses of hunted animals rot next to a stream. In the shadows of the tumbled walls, it takes very little digging to find the remains of bronze shield facings, broken blades, maybe a few ancient coins, and fresh bones. The relics draw scavengers of all races and reasons, and once captured by the Valley, very few seem to make it back out of their own volition.<br/><br/>\"What drew Lord Tellum's son to this godforsaken area?\”<br/><br/>It's a question all of you are asking. Not that the answer really matters. After your heroic stand defending the village, word arrived from local nobility that Christos Tellum, heir to Tellum's Hold up near Waterdeep, was overdue from an excursion through the Valley. Not only Christos, but eight guards, Qellon the Arcane, and Ardin, a local ranger as guide.<br/><br/>\“It would be in our best interest—and your beneficial gain—were you to find the lost party.\"<br/>Duchess Daggerford herself signed the parchment, making it as much an order as a request.<br/>But a single glance into the Valley ahead guarantees one thing: it will be difficult traveling.",
                 ko: "데사린 계곡.<br/><br/>억센 덤불과 가시 덩굴들이, 그것이 마치 자기 것인 것 마냥, 부서진 고대의 탑을 뒤덮어 숨기고 있었다. 최근에 있었던 숲의 화재로 불에 탄 땅에는 시체먹는 동물들을 위한 그을린 뼈들이 널려있었다. 당신은 찔레 덤불과 독이 있는 열매들로 뒤덮힌 계곡의 보물 사냥꾼들을 위한 사냥 길을 뚫고 올라갔다.<br/><br/>\"여기에서 많은 피가 흘렀군.\"<br/><br/>누구나 알 수 있는 일이었다. 황무지의 어두운 그림자 속에는 당신을 지켜보는 눈들이 있고, 사냥당한 동물의 시체들은 시냇물 옆에서 썩고 있었다. 무너진 벽의 그림자 아래에서 청동 방패 조각, 부러진 칼날, 몇 개의 고대 동전, 그리고 생긴지 얼마 안 된 뼈의 잔해를 찾는 것은 어렵지 않다. '유물'은 종족과 이유를 불문하고 보물 사냥꾼들을 끌어들였고, 한 번 계곡에 발을 들인 자들 중 대부분은 자신의 의지로 돌아가지 못했다.<br/><br/>\"텔럼경의 아들이 이 신에게 버림받은 곳에 온거지?\"<br/><br/>모두가 가진 의문이었지만, 그 답은 중요하지 않았다.  당신이 마을을 지키는 영웅적인 활약을 한 뒤, 워터딥 근처 텔럼 성채의 후계자인 크리스토스 텔럼에 대한 소식이 전해졌다. 그와 8명의 가드, 비전술사 퀠른 그리고 순찰자인 아딘으로 이루어진 일행은 이미 계곡을 지나 목적지에 도착했었어야 했다.<br/><br/>\"서둘러 그들을 찾아야 합니다. 보상은 충분히 하겠어요.\" 대거포드의 여공작은 직접 양피지에 서명하며, 사실상 명령이나 다름 없는 부탁을 했다. 눈 앞에 놓인 계곡을 보면 확실한 것이 하나 있다. 그것은 이 여행이 쉽지 않을 것이라는 것이다."
@@ -9,26 +24,54 @@ var data_dragonfire = [
                 en: "As the final body falls, its blood staining the ground dark, a long figure crashes its way through nearby brush and briars. Sword in hand, it staggers forward, rocking from side to side as if to a chant only it can hear. Tired arms brandish weapons. Fresh spells hover on lips. As Lord Tellum's son and heir all but falls into your arms.<br/><br/>\"Mine! And make no mistake about it!\"<br/><br/>His once-rich clothing now hangs in tatters, and his face is drawn and haggard. If he's slept in the past three days, it wasn't for long. In one hand he grips what appears to be a simple iron flask. With his other, he slashes a sword in your direction, but there's no strength behind it. Doubt clouds his eyes for a moment, and he lowers the blade.<br/><br/>\"No. Not bandits. No. Did he send you? Duchess Daggerford? Or... my father? Mine, tell them..\"<br/><br/>It's the last coherent sentence he forms. And, dropping his sword in the battlefield’s gore, he collapses.Gathered around the young noble, muscles still aching from the recent battle, there seems little you can do except to make camp and watch over him. The valley seems safe enough—at the moment—for a small fire and a rest. Today, at least, your actions have made the difference. And tomorrow will take care of tomorrow.<br/><br/>Still, the young lord's last word echoes in your head: \"..mine.\"",
                 ko: "마지막 적이 쓰러지며 그것의 피가 땅을 검게 물들이는 순간, 긴 형체 하나가 근처의 덤불과 찔레가시를 뚫고 나왔다. 그것은 비틀거리며 움직이고 있었고, 손에 든 검은 마치 자신만이 들을 수 있는 주문을 외는 듯 좌우로 흔들리고 있었다. 무기를 휘두르는 팔에는 힘이 없었고, 주문을 외는 듯 입술을 웅얼거리고 있었다. 텔럼 경의 아들이자 후계자인 그는 쓰러지듯 당신의 팔에 안겼다.<br/><br/>\"이건 내꺼야! 허튼 생각하지 마!\"<br/><br/> 원래는 고급스러웠던 것이 분명한 그의 옷은 이제 넝마가 되어 있었고, 그의 얼굴은 핼쑥하고 초췌했다. 마치 3일은 잠을 못 잔 듯 하다. 그는 한 손에 단순한 모양의 철제 플라스크를 쥐고 있었고 다른 한 손으로는 당신을 향해 검을 휘두르고 있었지만, 힘 없이 휘두르는 무기는 위협이 되지 않았다. 잠시 의심의 눈초리로 당신을 보던 그는 곧 검을 내렸다.<br/><br/>\"아니. 도적은 아니군. 누군가 너희를 보냈나? 대거포드의 여공작? 아니면 아버지? 이건 내꺼야. 그들에게 전해...\"<br/><br/> 이 말을 마지막으로 그는 전장의 시체들 위로 검을 떨어뜨리며 무너져 내렸다. 전투의 후유증으로 인한 근육통을 느끼며, 쓰러진 젊은 귀족 곁으로 모였다. 당장 할 수 있는 일은 캠프를 만들고 그를 지켜보는 것 밖에 없는 듯 하다. 계곡은 지금 당장은 작은 모닥불을 피우고 휴식을 취할 수 있을 만큼 안전해 보인다. 어쨋든 오늘은 헛수고를 하진 않은 것 같다. 내일 일은 내일 걱정하는게 나으리라.<br/><br/>젊은 후계자의 마지막 말이 계속해서 당신의 머리속에 울려 퍼진다.<br></br>\'...이건 내꺼야\'"
         },
-		{
-                name: "BODYGUARDS: PROLOGUE",
-                en: "With a night's rest and a decent meal behind him, Christos Tellum seems a new man.<br>
-Almost...
-"It's a good thing we understand each other. No chance for a mistake." He starts to draw his sword, reconsiders, and rams it violently back into the sheath.
-"Sorry. Sorry. It's just cost me so much already. Ten good guards. They should have been stronger! Such a high price. What it must be worth..."
-He refers to a treasure recovered from the Dessarin Valley. What looks like a simple iron flask. The first thing he looked for upon waking from his desperate sleep. The last thing he'd allow one of you to carry.
-“High magic. That's what Q’ellon called it. And he would know. Ancient sorcery.”
-That much you've seen for yourself. Or, at least, so says your wizard. A powerful aura of magic surrounds the flask. “The cold, negative dweomer of abjuration." That much is certain. “And perhaps a layered cocoon of conjuration..." Whichever the case, the aura doubles in strength when young Christos pries at the cap, resisting him.
-"A reward!” Christos shouts, his strained voice carrying over the brush-strewn land. “You’ll escort me back to Daggerford at once. Before they come for me. By my father's command, you will!”
-Lord Tellum's command or no, the result of leaving the unstable man in the more remote regions of the valley is clear. And Duchess Daggerford's intent was quite clear. For whatever favor she owes Lord Tellum, she wants his heir returned intact. A three-day march, at best, but returning Christos Tellum to Daggerford is clearly your only choice.
-",
+        {
+                name: "# BODYGUARDS",
+                en: "",
+                ko: "당신은 데사린 계곡의 깊은 곳에서 텔럼경의 아들을 구출했다. 이제 당신은 그를 호위하여 대거포드로 무사히 데려가야한다. 그렇게 하지 못하면 텔럼즈 홀드의 영지는 분열될 것이다. 당신은 아마도 이 괴짜 귀족의 기묘한 행동들을 쉽게 참아낼 수 있을 것이다. 그의 아버지, 그리고 대거포드의 여공작을 생각한다면 말이다."
+        },
+	{
+	        name: "BODYGUARDS: PROLOGUE",
+	        en: "With a night's rest and a decent meal behind him, Christos Tellum seems a new man.<br/><br/>Almost...<br/><br/>\"It's a good thing we understand each other. No chance for a mistake.\" He starts to draw his sword, reconsiders, and rams it violently back into the sheath.<br/><br/>\"Sorry. Sorry. It's just cost me so much already. Ten good guards. They should have been stronger! Such a high price. What it must be worth...\"<br/><br/>He refers to a treasure recovered from the Dessarin Valley. What looks like a simple iron flask. The first thing he looked for upon waking from his desperate sleep. The last thing he\'d allow one of you to carry.<br/><br/>\“High magic. That's what Q’ellon called it. And he would know. Ancient sorcery.\”<br/><br/>That much you've seen for yourself. Or, at least, so says your wizard. A powerful aura of magic surrounds the flask. \“The cold, negative dweomer of abjuration.\" That much is certain. \“And perhaps a layered cocoon of conjuration...\" Whichever the case, the aura doubles in strength when young Christos pries at the cap, resisting him.<br/><br/>\"A reward!\” Christos shouts, his strained voice carrying over the brush-strewn land. \“You’ll escort me back to Daggerford at once. Before they come for me. By my father's command, you will!\”<br/><br/>Lord Tellum's command or no, the result of leaving the unstable man in the more remote regions of the valley is clear. And Duchess Daggerford's intent was quite clear. For whatever favor she owes Lord Tellum, she wants his heir returned intact. A three-day march, at best, but returning Christos Tellum to Daggerford is clearly your only choice.",
+	        ko: ""
+                        /*
+                        "밤 동안의 휴식과 제대로 된 식사를 한 크리스토스 텔럼은 마치 딴 사람으로 보입니다.<br/><br/>
+거의...<br/><br/>
+\"우리가 서로를 이해하게 된 것은 좋은 일이다. 실수 할 일은 없어.\" 그는 칼을 꺼냈다가, 한참 생각하다 거칠게 칼집에 집어 넣습니다.<br/><br/>
+\"미안. 미안. 이미 너무 많은 대가를 치렀어. 열명의 훌륭한 가드들. 그들은 더 강해야 했어! 그렇게 돈을 많이 받았으면 그만한 가치가 있어야 할거 아니야...\"<br/><br/>
+그는 데사린 계곡에서 발견한 보물을 가리켰습니다. 그것은 단순한 철제 플라스크처럼 보입니다. 필사적인 잠에서 깨어날 때 가장 먼저 찾은 물건입니다. 마지막으로 한일은 당신들 중 한 명이 나르는 것을 허용한 것입니다.<br/><br/>
+\"고위 마법. 퀠론은 이것을 그렇게 불렀다. 그리고 그는 알 거야. 고대의 술법. \"<br/><br/>
+그 만큼이 당신 스스로 본 것이다. 적어도 마법사는 이렇게 말합니다. 강력한 마법의 오라가 플라스크를 에워 싸고 있습니다.  \"어브쥬레이션(보호학파)의 차갑고 부정적인 드웨이오머(오라-인챈트된 아이템에서 나타나는). \"그것만큼은 확실합니다. \"그리고 어쩌면 계층으로 둘러싸인 주문...\" 어쨌든, 오라는 젊은 크리스토스가 모자를 잡을 때 두배로 강해지며, 그에게 저항합니다..<br/><br/>
+        \"보상!\" 크리스토스가 소리칩니다. 그의 긴장된 목소리가 덤불이 흩어진 땅위로 퍼집니다. \"나를 즉시 대거포드로 데리고 돌아가라. 그들이 오기 전에, 내 아버지의 명령에 따라, 너는 해야한다.!\"<br/><br/>
+        로드 텔럼의 명령이건 아니건, 불안정한 사람을 골짜기의 더 깊은 지역에 남겨 두는 결과는 분명합니다. 그리고 대거포트 공작부인의 의도는 명확합니다. 그녀가 로드 텔럼에게 빚진 것은 무엇이든 간에, 그녀는 그의 상속인이 그대로 돌아왔으면 합니다. 기껏해야 3 일간의 행군이지만, 크리스토스 텔럼을 대거 포드로 돌려 보내는 것만이 당신이 유일하게 선택할 수 있는 일입니다.
+        */
+	},
+        {
+                name: "BODYGUARDS: EPILOGUE",
+                en: "Your opponents falter as you break into the clear. The gates of Daggerford stand open, beckoning through the Sword Coast's dusky twilight. Shouts from the watch greet you, along with the ringing alarm of the militia.<br/><br/>Snarls and shouts chase you forward, the enemy reluctant to give up their prize. A voice from behind shouts for, \"Nadir? Nadir!” but no one answers. Others cast about, searching for a leader.<br/><br/>A flight of arrows from Daggerford’s ramparts convinces the enemy there will be no reward for them this day. First one, then another, stagger back toward the forest's dark embrace. Finally, all opposition collapses. The wounded limp away, guarded by the strong. And you all but carry Lord Tellum's son through Daggerford's heavy iron gates.<br/><br/>Inside, you are greeted by Sir Isteval. Limping forward on the support of a thick cane, his leg wrapped in a heavy brace, every militia member steps aside for him. A retired Purple Dragon Knight, he's the closest thing this stretch of the Sword Coast has to a military commander.<br/><br/>“You found him! Praise the makers and their servants!\"<br/><br/>Hands reach out to relieve you of your charge. The moment a militia member touches Christos Tellum, however, he staggers away with a shout and a snarl of rage twisting his face.<br/><br/>“Unhand me, vermin! Or find yourself cast into the fires and charred to a cinder!\" He raises the iron flask above his head, as if ready to bludgeon the first person to approach him. “Find me Nadir!\"<br/><br/>Sir Isteval recoils as if gut-punched. Glances into the nearby shadows. Studies the face of each nearby person—and the faces of your party closest of all.",
                 ko: ""
+                /*
+                상대방은 당신이 들어가는 순간 비틀거립니다.  대거 포드의 문이 열리고, 소드 코스트의 어스름한 황혼이 손짓합니다. 민병대의 경고가 울리고 감시자의 외침이 당신을 반깁니다.<br/><br/>
+울부짓는 소리와 고함이 당신 앞으로 몰려듭니다. 적들은 자신의 먹이를 포기할 생각은 없습니다. 누군가가 뒤에서  \"나디어? 나디어!\" 라고 외쳤지만 아무도 대답하지 않습니다. 다른 사람들이 찾습니다. 리더를 찾습니다.<br/><br/>
+대거 포드의 성벽에서 날아온 화살이 적들에게 오늘은 그들에게 보상이 없을 것이라는 것을 알려줍니다. 첫번째, 그리고 다른 하나는 숲의 어둠속으로 비틀거리며 들어갔습니다. 마침내 모든 경쟁자들이 무너졌습니다. 상처입은 젊름발이들은 가고, 강하게 보호받습니다. 그리고 당신들은 대거포드의 무거운 철문을 통해 로드 텔럼의 아들을 만날 수 있습니다.<br/><br/>
+내부, 당신에게 이스터발 경이 인사합니다. 두꺼운 지팡이를 짚고 절뚝거리며 나옵니다. 그의 다리는 무거운 보조기에 싸여 있습니다. 모든 민병대원은 그의 뒤에 물러나 있습니다. 은퇴 한 퍼플 드래곤 나이트, 그는 소드 코스트의 이 순간에 군 사령관에 가장 가까운 것입니다.<br/><br/>
+\"당신이 그를 찾았습니다! 해낸 이들과 그들의 종자들에게 찬사를 보냅니다. \"<br/><br/>
+손을 내밀어 당신을 임무에서 벗어나게 해줍니다. 민병대원이 크리스토스 텔럼에게 손을 대는 순간, 그는 일그러진 얼굴로 소리치면서 비틀거리며 벗어났습니다.<br/><br/>
+\"내게서 손때, 벌레 같은 놈! 아니면 시커멓게 탄 채로 불 속에 있는 너를 보게 될거야! \" 그에게 다가오는 사람을 때릴 준비를 하는 것처럼 철제 플라스크를 머리 위로 들어 올립니다.<br/><br/>
+\"나를 찾아 나디어! \"<br/><br/>
+이스터발경은 한대 맞은 것처럼 움찔거립니다. 근처의 그림자를 힐끗 봅니다. 근처에 있는 모든 사람의 얼굴과 가장 가까운 사람의 얼굴을 자세히 봅니다.
+                */
+        },
+        {
+                name: "# UNHOLY PRESENCE",
+                en: "",
+                ko: "뱀파이어가 대거포드의 거주지를 점령했고, 부정한 언데드들이 거리를 활보하고 있다. 당신은 이 곳에 도착하면 텔럼경의 아들이 안전해지리라 생각했으나, 아직은 아니었다."
         },
 ];
 
 /*
-{
-                        name: "",
-                        en: "",
-                        ko: ""
-                },
+        {
+                name: "",
+                en: "",
+                ko: ""
+        },
 */
