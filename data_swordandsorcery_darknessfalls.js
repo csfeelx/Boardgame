b84f329b25a52d8626c7bf0d0f61746f4b743fe6
DESKTOP-OH4RS9J\csp <csp@pubple.com>
2018-12-04 11:42:03 +0900

    no message

---
+++ data_swordandsorcery_darknessfalls.js
@@ -0,0 +1,45 @@
+var data_swordandsorcery_darknessfalls = [
+        {
+                name: "ACT II - QUEST I : Nothing is as good as it seems : Introduction",
+                en: "The moon shines high, bathing Twin Wyrms in silvery light.<br/><br/>The roads are teeming with life, waiting for the clarion call to begin the festival held in honor of the Immortal Souls.<br/><br/>The crowd cheers as the Heroes march to the Royal Palace, honored guests at the grand banquet held by Count Dalv himself and attended by all the nobility of the kingdom. Although the castle suffered damage in the siege, it seems as mighty as ever during this happy and glorious night; the Heroes enter the hall and, as the doors close behind them, Count Dalv proclaims, \"Let the dance begin!\"",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST II : In the Forest : Introduction",
+                en: "The Heroes, along with Marcus, journey to Moonchild; their path will pass through the forest, avoiding nearby Lone Pine. The journey unfolds without incident, yet as they catch a glimpse of dense black smoke rising from southwest, the Heroes sadly acknowledge the terrible fate of Lone Pine. While they have avoided any toruble because of the detour, nobody is actually happy. A problem avoided is rarely a problem solbed. As if to confirm the sentiment, the forest becomes enshrouded in the mountains' dark shadows.",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST III : The Darkness' Army : Introduction",
+                en: "The Heroes, together with Marcus, head toward Lone Pine, a small town due south of Twin Wyrms, looking for answers. After several hours on the trail, the group encounters the first refugees; desperate people trying to escape toward the city of the Immortal Heroes, hoping for solace and help. When the Heroes question one of the refugees, he tells the tale of his doomed city. \"Go no further, the town is cursed! When the dead rise from their graves, there is no place for the living. Run, you fools!\" As the party approaches the town, more and more refugees flood the road, and the atmosphere becomes tense. In silence, the Heroes reach the gates of the town and are welcomed by screams from the nearby central square!",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST IV : Moonchild : Introduction",
+                en: "At last the Heroes reach the gates of Moonchild. Spectral clouds cover the moon's silver sickle in the sky, and the walls are manned by torch-bearing guards. One of them gets sight of the Heroes and yells, \"Stangers! You are welcome, but know that this city is expecting an undead siege anytime now, coming from the South. Entering Moonchild means tempting your own fate!\" The gates open, and Marcus introduces the Heroes, asking to speak to the mayou. Yet the guard, visivly upset, answers, \"The town hall will not open until tomorrow morning. You can talk to Inspector Crane, near to the walls of the next block. He is examining the scene of yet anothr heinous crime...\" <b>See Paragraph $4.1.</b>",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST V : Nothing is as bad as it seems : Introduction",
+                en: "The party journeys to Count Dalv's castle, along the shores of Lake Scythe. With the undead armies engaged in their march toward Moonchild, the Heroes pass by undetected. The small castle, built on top of a sheer cliff on the sea, is silhouetted against the starry sky like an evil, cloaked figure. A thick shoroud of dard clouds surrounds the castle, isolating it from the rest of the world. Marcus stops the party just outside of the shroud and speaks: \"A new day will soon dawn, yet this cloud blocks our greatest ally against the vampire count... the sun! I will not be any help in the castle rooms, but form here - with the help of the gods - I will try to weaken the enshrouding sorcery. Should I succeed, you will have word from me. May the Three bring you victory!\" And so, the Heroes advance toward the door. Only silence comes from inside, because Death makes no noise.",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST VI : Cemetery Gates : Introduction",
+                en: "After meeting with Marcus, the party decides to enter the portal opened upon the death of Count Dalv. Black magical tendrils painfully envelop the Heroes, teleporting them to a distant land, far away from the count's castle. The cold stone is lit by weak torches, and the damp, pungent air of the undergound, are all the Heroes see and feel. Without another hint, the party parys to the gods to help them in their quest, and begins exploring the gloomy dungeon forking just ahead.",
+                ko: ""
+        },
+		{
+                name: "ACT II - QUEST VII : temple of Doom! : Introduction",
+                en: "Finally, the entrance to the temple, a pryramid of fear, the dominion of the nameless evil, lies within reach. Just before stepping through the threshold, all the Soul Gems light up and each hero has a vision of his god appearing as an ethereal presence, silent and grave; the divine avatars. Each avatar bestows items to the hero he communicates with, holy gifts for the imminent battle. Then, the vision fades as the gods nod in benevolent farewell.(See <b>Emporium Special Quest Rules</b>). Marcus witnesses the scene, then kneels down and prays. Then the cleric utters solemn words. \"The gods are with us. Beyound that door you will find your freedom and the salavation of the Talon Coast! We are not alone at the end of this journey! Holy gods, please be our guides, before and after this last endeavor. We are your soldiers serving the light, forever fighting brave and proud!\" With these words, and bathed in the benevolent light of the Three, the Heroes step into the darkness of the temple of doom...",
+                ko: ""
+        },
+];
+
+/*
+        {
+                name: "",
+                en: "",
+                ko: ""
+        },
+*/
