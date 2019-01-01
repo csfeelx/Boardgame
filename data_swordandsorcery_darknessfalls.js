2dd0ebf8f5aceb6d4200f38b0c64f2365d720ce2
csfeelx <csp@pubple.com>
2019-01-01 12:57:42 +0900

    no message

--- data_swordandsorcery_darknessfalls.js
+++ data_swordandsorcery_darknessfalls.js
@@ -1,8 +1,73 @@
-var data_swordandsorcery_darknessfalls = [
+﻿var data_swordandsorcery_darknessfalls = [
         {
                 name: "ACT II - QUEST I : Nothing is as good as it seems : Introduction",
                 en: "The moon shines high, bathing Twin Wyrms in silvery light.<br/><br/>The roads are teeming with life, waiting for the clarion call to begin the festival held in honor of the Immortal Souls.<br/><br/>The crowd cheers as the Heroes march to the Royal Palace, honored guests at the grand banquet held by Count Dalv himself and attended by all the nobility of the kingdom. Although the castle suffered damage in the siege, it seems as mighty as ever during this happy and glorious night; the Heroes enter the hall and, as the doors close behind them, Count Dalv proclaims, \"Let the dance begin!\"",
-                ko: ""
+                ko: "높게 드리운 달빛이 트윈 웜즈를 은빛으로 물들이고 있다.<br/><br/>거리는 불멸의 영웅들을 찬양하기 위한, 축제의 시작을 기다리는 사람들로 넘쳐났다.<br/><br/>영웅들은 달브 백작이 주최하는, 왕국의 모든 귀족들이 참석한 대연회의 명예로운 손님이었고, 군중들은 이를 위해 궁전으로 행진하는 영웅들에게 환호를 보냈다. 성은 지난번 공격에 의한 피해를 받긴했지만, 이 행복하고 영광스러운 밤 동안에는 그 어느때보다 웅장해보였다. 영웅들이 홀에 입장하고 그 뒤로 문이 닫히자, 달브 백작은 축제의 시작을 선포했다. \"자. 모두 춤을 춥시다!\""
+        },
+		{
+                name: "$1.1 (Waypoint 1)",
+                en: "The castle's main hall hosts a crowd of people, many of whom seem interested in the heroes who just walked in...",
+                ko: "성의 중앙 홀은 사람들로 가득차있다. 그들중 많은 수는 방금 등장한 영웅들에게 흥미가 있어 보인다..."
+        },
+		{
+                name: "$1.2 (Waypoint 1)",
+                en: "One of the veteran guards steps in front of you, salutes, and say, \"It has been an honor to fight beside you, Sir. Glory to you, and to my fallen comrades!\" Then, he vigorously shakes your hand and takes his leave, bowing deeply.",
+                ko: "고참 경비병 하나가 당신 앞으로 다가와, 경례를 한 뒤 말했다. \"당신과 함께 싸울 수 있었던 것은 명예로운 일이었습니다. 쓰러진 동료들과 당신께 영광을!\" 그리고 그는 당신과 힘차게 악수를 나눈 뒤, 깊숙히 허리를 숙이며 작별을 고했다."
+        },
+		{
+                name: "$1.3 (Waypoint 1)",
+                en: "Dancers and jugglers run before you and, in your honor, perform for several minutes!",
+                ko: "무희들과 광대들이 당신을 찬양하는 의미로, 몇 분동안 당신을 뒤 따르며 재주를 선보였다."
+        },
+		{
+                name: "$1.4 (Waypoint 1)",
+                en: "A charming, long-haired man in noble garments approaches and, after honoring your achievements, offers you a deal: \"My name is Lestat, and I would dare yo make you a... singular offer: 50 Crowns in exchange for a few drops of your immortal blood. It requires a bit of pain, but... it would be nothing compared to your previous trials!\"",
+                ko: "긴머리의 매력적인, 귀족 남성 하나가 당신에게 다가왔다. 그는 당신의 업적을 찬양하며, 당신에게 제안 하나를 했다. \"제 이름은 레스타트입니다. 무례를 무릅쓰고 당신에게... 특이한 제안 하나를 하고 싶군요. 50 크라운으로 당신이 가진 불멸의 피를 몇 방울 사고 싶습니다. 조금 아플수도 있겠지만... 당신이 이전에 겪었던 전투와 비교하면 아무것도 아닐겁니다!\""
+        },
+		{
+                name: "$1.5 (Waypoint 1)",
+                en: "Strangely, many of the guests keep their distance from you - either out of respect or fear...",
+                ko: "이상하게도, 손님들 중 많은 수가 당신과 거리를 두고 있다. - 존경 또는 공포 때문인 듯 하다."
+        },
+		{
+                name: "$1.6 (Waypoint 1)",
+                en: "Lestat gives you the crowns and then gashes your wrist with a sharpened thumb ring, draining a few drops of your blood into a glass vial.<br/><br/>You feel a strange sensation; the pain was too intense for such a small puncture... but, before you can say anything, Lestat fades away among the other guests.",
+                ko: "레스타트는 당신에게 크라운을 건내준 뒤, 엄지 손가락에 끼고 있던 날카로운 반지로 당신의 손목을 찔러, 당신의 피 몇방울을 유리병에 담았다.<br/><br/>당신은 묘한 기분이 들었다. 작은 상처의 크기에 비해 고통이 너무 강렬했던 것이다... 하지만, 이에 대해 묻기도 전에, 레스타트는 다른 손님들 사이로 사라지고 없었다."
+        },
+		{
+                name: "$1.7 (Waypoint 2)",
+                en: "Marble staircases form the huge plinth of the King's Throne. A finely carved, gilded seat bears the names of all the past Kings who ruled from this place of power.",
+                ko: "대리석 계단이 옥좌로 오르는 거대한 주춧돌을 이루고 있다. 훌륭하게 세공된 화려한 의자에는 이 곳을 지배한 선대 군주들의 이름이 새겨져있었다."
+        },
+		{
+                name: "$1.8 (Waypoint 2)",
+                en: "You take a closer look and immediately notice the name of King Rastan has been recently carved, while Rygar - the missing son - is absent, almost as if representing the feeble, yet living, hope of his people. The noble pride of Count Dalv seems untarnished by his role as ruler without a crown.",
+                ko: "자세히 살펴보자 최근에 새겨진 라스탄 왕의 이름이 있는 것을 알 수 있었다. 그의 실종된 아들 라이가의 이름은 목록에 없었는데, 그것은 마치 그의 백성들의 미약한 희망대로 그가 아직 살아있기 원하는 것을 나태내고 있는 듯 했다. 달브 백작은 그가 섭정으로서 절대자가 되었음에도 불구하고 귀족으로서의 긍지를 잊지 않은 듯 했다."
+        },
+		{
+                name: "$1.9 (Waypoint 3)",
+                en: "A huge fresco of King Rastan seated on this throne, wearing the Jeweled Crown of Twin Wyrms on his troubled brow, overlooks the rise.",
+                ko: "거대한 벽화에 그려진 보석으로 장식된 왕관을 쓴 근심 가득한 표정의 라스탄 왕은, 당신의 행동을 묵과하고 있다."
+        },
+		{
+                name: "$1.10 (Waypoint 3)",
+                en: "A sudden glimpse of movement among the shadows reveals a gorgeous lady with fiery red hair. She also becomes aware of your presence and approaches you, stating, \"I certainly cannot say I loved this King, yet I must admit his absence has caused troubles both to this city.... and my affairs. Please, accept this...\" She offers you a goblet full of red wine and suddenly disappears into the crowd.",
+                ko: "그림자 사이에서 인기척을 느낀 당신은 불타는 듯한 붉은 머리를 가진 화려한 외모 귀부인을 발견했다. 그녀 역시 당신의 존재를 눈치채고, 당신에게 다가와 말했다. \"내가 왕을 사랑했다고 확언하긴 어렵군요. 하지만 그의 부재가 이 도시와... 나에게 큰 문제가 되었다는 건 인정해야겠네요. 부디, 거절하지 말아주세요.\" 그녀는 당신에게 적포도주가 가득찬 술잔을 건네준 뒤, 홀연히 사람들 속으로 사라졌다."
+        },
+		{
+                name: "$1.11 (Waypoint 4)",
+                en: "The upper hall is decorated with many art objects from the whole kingdom.",
+                ko: "위쪽 홀은 왕국 각지에서 가져온 수 많은 미술품들로 장식되어 있다."
+        },
+		{
+                name: "$1.11 (Waypoint 4)",
+                en: "Among the art objects, you notice a strange wooden crate carved with dragon effigies and surrounded by black metal plates at the edges. This wooden artifact emits a faint magical aura, almost impossible to detect.",
+                ko: "미술품들 속에서 당신은 이상한 나무 상자 하나를 발견했는데, 그것은 용을 묘사한 그림이 새겨져 있었고, 모퉁이는 검은 철판으로 둘러쌓여 있었다. 그 나무 공예품은 거의 감지하기 불가능할 정도의 미약한 마법의 오오라를 내보내고 있었다."
+        },
+		{
+                name: "$1.12 (Waypoint 4)",
+                en: "As you try to inconspicuously open the crate, its very sharp edge cuts you slightly, dropping some of your blood inside the box. The drops disappear. avidly absorbed by the dirt inside. The strange dirt smells old, and the magical aura is but the echo of something very powerful once transported inside this crate.",
+                ko: "당신이 주위의 눈을 피해 상자를 열었을 때, 당신은 상자의 아주 날카로운 모서리에 베이고 말았다. 당신이 흘린 피 몇방울이 상자의 안으로 떨어졌는데, 그것은 안에 있던 먼지에 순식간에 흡수되어 사라졌다. 그 이상한 먼지는 오래된 냄새를 풍기고 있었고, 마법의 오오라는 단지 한때 상자에 담겨있었던 강력한 무언가의 잔향일 뿐이었다."
         },
 		{
                 name: "ACT II - QUEST II : In the Forest : Introduction",
