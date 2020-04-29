aadd9b14e18bef5a9b46a6bdbc1fcc8c61d5765f
DESKTOP-OH4RS9J\csp <csp@pubple.com>
2020-04-29 14:44:46 +0900

    no message

---
+++ gloomhaven.js
@@ -0,0 +1,12 @@
+﻿var data_gloomhaven = {
+        City: ["city 1", "city 2", "city 3", "city 4", "city 5"],
+        Road: ["road 1", "road 2", "road 3", "road 4", "road 5"],
+        Personal: ["personal 1", "personal 2", "personal 3", "personal 4", "personal 5"],
+        Expansion: [{
+                name: "Expansion Scenario #1",
+                text: "Detail #1"
+        }, {
+                name: "Expansion Scenario #2",
+                text: "Detail #2"
+        }]
+}
